b7bf21b2a92e4b4036bbf28ef4e7e5b5364765a2
Hamza Najfi <141533696+asli-nobita@users.noreply.github.com>
2026-09-19 12:22:59 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int l = 0, r = n - 1;
+        int count = 0;
+        sort(people.begin(), people.end()); 
+        while (l <= r) {
+            if (l == r) { 
+                count++; 
+                break; 
+            }
+            else {
+                if (people[l] + people[r] <= limit) {
+                    count++;
+                    l++;
+                    r--;
+                } else {
+                    count++;
+                    r--;
+                }
+            }
+        }
+        return count;
+    }
+};
